e37f9ccd98e773a7433ec0c9c3ca4e3741a0bd74
anniegriebie <GRIEB046@umn.edu>
2023-03-19 13:27:47 -0500

    Adding filters to shiny data and output plot

--- shiny_week8/app.R
+++ shiny_week8/app.R
@@ -14,6 +14,7 @@ ui <- fluidPage(
           #Added option to include or exclude participants who completed assignement before given date, with exclude set to default
           selectInput("date_completed", "Include or Exclude paticipants who completed assessment before August 1, 2017", choices=c("Include", "Exclude"), selected="Exclude"),
         ),
+        #selected plot to show scatter plot option
         mainPanel(
           plotOutput("plot")
         )
@@ -21,5 +22,27 @@ ui <- fluidPage(
 )
 
 #Server function
-server <- function(input, output)
+server <- function(input, output) {
+  
+  #using readRDS to rename skinny data
   shiny_data<-ReadRDS("shiny_data_week8.rds")
+  
+  #adding expression to generate scatter plot
+  output$plot = renderPlot({
+   
+     shiny_data%>%
+    #Adding option to filter gender when select something other than default "All"
+    if(input$gender != "All") {
+      filter(gender=input$gender)
+    }
+    #Adding option to filter participant finishing date
+    if(input$error_band != "Include") {
+      filter(timeEnd < "2017-08-01 00:00:00")}
+    
+    #Adding option to have or exclude error band
+     })
+      
+    }
+  
+# Run the app
+  shinyApp(ui =ui, server= server)
